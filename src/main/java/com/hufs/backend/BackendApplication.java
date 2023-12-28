88257d387f12408445658504d3a2bf309b6a887a
deepsj1012 <deepsj1012@naver.com>
2023-12-28 14:01:47 +0900

    Test push

--- src/main/java/com/hufs/backend/BackendApplication.java
+++ src/main/java/com/hufs/backend/BackendApplication.java
@@ -7,8 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BackendApplication {
 
     public static void main(String[] args) {
-
+        //푸시 확인 주석
         SpringApplication.run(BackendApplication.class, args);
     }
-
 }
