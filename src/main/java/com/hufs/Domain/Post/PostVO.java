ebc4fa81e3df8760656edcd049e68e869b871f94
deepsj1012 <deepsj1012@naver.com>
2023-12-28 17:36:33 +0900

    PostVO 클래스의 불필요한 주석 제거

--- src/main/java/com/hufs/Domain/Post/PostVO.java
+++ src/main/java/com/hufs/Domain/Post/PostVO.java
@@ -1,18 +1,5 @@
 package com.hufs.Domain.Post;
 
 public class PostVO {
-    //sample
-    //develop branch push 확인
-    //develop branch push 확인2
-    //develop branch push 확인3
-    //develop branch push 확인4
-    //develop branch push 확인5
-    //develop branch push 확인6
-    //develop branch push 확인7
-    //develop branch push 확인88
-    //develop branch push 확인9
-    //develop branch push 확인10
-    //develop branch push 확인11
-    //develop branch push 확인12
-    //develop branch push 확인13
+
 }
